1e25fa74db67f77729353d16a320b70613eeade9
hussainkanafani <hussainkanafani@gmail.com>
2023-07-19 18:00:49 +0200

    fix flake issues

--- app.py
+++ app.py
@@ -1,28 +1,28 @@
 from fastapi import FastAPI
 from fastapi.responses import PlainTextResponse
-#from initializer import LoggerInstance
 from fastapi.routing import APIRouter
-from transformers import pipeline
-import zipfile
-from inference_pipeline import InferencePipeline
 import logging
 import logging.config
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+# from initializer import LoggerInstance
+# from transformers import pipeline
+# import zipfile
+# from inference_pipeline import InferencePipeline
 
-app= FastAPI(title="text summarizer", version="0.1")
+app = FastAPI(title="text summarizer", version="0.1")
 
 inference_pipeline = None
 
 router = APIRouter()
-#logger = LoggerInstance().get_logger(__name__)
+# logger = LoggerInstance().get_logger(__name__)
 logging.config.fileConfig('logging_config.ini')
 logger = logging.getLogger(__name__)
 
-gen_kwargs = {"length_penalty": 0.8, "num_beams":8, "max_length": 128}
+gen_kwargs = {"length_penalty": 0.8, "num_beams": 8, "max_length": 128}
 
-#def load_model_files(zip_file):
-#    with zipfile.ZipFile(zip_file,'r') as zip_ref:
-#        zip_ref.extractall('unzipped_model')
+# def load_model_files(zip_file):
+#     with zipfile.ZipFile(zip_file,'r') as zip_ref:
+#         zip_ref.extractall('unzipped_model')
 
 
 @app.on_event("startup")
@@ -30,13 +30,10 @@ async def on_startup():
     logger.info('start server')
     global inference_pipeline
     logger.info('load model')
-    
-    #uncomment when to use your finetuned model
-    #inference_pipeline=InferencePipeline().inference_pipe
-
-
+    # uncomment when to use your finetuned model
+    # inference_pipeline=InferencePipeline().inference_pipe
     global tokenizer, model
-    checkpt="google/pegasus-cnn_dailymail"
+    checkpt = "google/pegasus-cnn_dailymail"
     tokenizer = AutoTokenizer.from_pretrained(checkpt)
     model = AutoModelForSeq2SeqLM.from_pretrained(checkpt)
     logger.info('app is ready!')
@@ -44,17 +41,17 @@ async def on_startup():
 
 @app.get("/")
 async def root():
-    return PlainTextResponse('Welcome to Pegasus Text Summarizer!',200)
+    return PlainTextResponse('Welcome to Pegasus Text Summarizer!', 200)
 
 
 @app.get("/predict")
-async def predict(text:str):
+async def predict(text: str):
     logger.info('start inference')
-    #uncomment to use your model
-    #output=inference_pipeline(text, **gen_kwargs)[0]["summary_text"]
+    # uncomment to use your model
+    # output=inference_pipeline(text, **gen_kwargs)[0]["summary_text"]
 
     # Generate Summary from pretrained model
     inputs = tokenizer([text], max_length=1024, return_tensors="pt")
     summary_ids = model.generate(inputs["input_ids"])
-    output=tokenizer.batch_decode(summary_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
-    return PlainTextResponse(output,200)
+    output = tokenizer.batch_decode(summary_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
+    return PlainTextResponse(output, 200)
--- inference_pipeline.py
+++ inference_pipeline.py
@@ -1,24 +1,22 @@
-#Prediction
 from transformers import pipeline, AutoTokenizer
 import yaml
 from dotmap import DotMap
 
-def load_yaml(path)-> DotMap:
-        with open(path,'r') as f:
-            file=yaml.safe_load(f)
-            return DotMap(file)
-            #return file
+
+def load_yaml(path) -> DotMap:
+    with open(path, 'r') as f:
+        file = yaml.safe_load(f)
+        return DotMap(file)
+
 
 class InferencePipeline():
     def __init__(self):
-        self.config= load_yaml('settings/config.yaml')
-        #print(self.config['tokenizer_path'])
-        self.inference_pipe=self.load_model(self.config)
+        self.config = load_yaml('settings/config.yaml')
+        self.inference_pipe = self.load_model(self.config)
 
-    def load_model(self,config):
-        tokenizer= AutoTokenizer.from_pretrained(config.tokenizer_path)
+    def load_model(self, config):
+        tokenizer = AutoTokenizer.from_pretrained(config.tokenizer_path)
 
-        pipe= pipeline("summarization", model=config.model_path, tokenizer=tokenizer)
-
+        pipe = pipeline("summarization", model=config.model_path,
+                        tokenizer=tokenizer)
         return pipe
-
