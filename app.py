dc531cfe6d1f3bc33ed2069cf6fdd148cf2e00ec
hussainkanafani <hussainkanafani@gmail.com>
2023-07-19 14:23:37 +0200

    modify app temporarily

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import zipfile
 from inference_pipeline import InferencePipeline
 import logging
 import logging.config
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 app= FastAPI(title="text summarizer", version="0.1")
 
@@ -29,7 +30,15 @@ async def on_startup():
     logger.info('start server')
     global inference_pipeline
     logger.info('load model')
-    inference_pipeline=InferencePipeline().inference_pipe
+    
+    #uncomment when to use your finetuned model
+    #inference_pipeline=InferencePipeline().inference_pipe
+
+
+    global tokenizer, model
+    checkpt="google/pegasus-cnn_dailymail"
+    tokenizer = AutoTokenizer.from_pretrained(checkpt)
+    model = AutoModelForSeq2SeqLM.from_pretrained(checkpt)
     logger.info('app is ready!')
 
 
@@ -41,5 +50,11 @@ async def root():
 @app.get("/predict")
 async def predict(text:str):
     logger.info('start inference')
-    output=inference_pipeline(text, **gen_kwargs)[0]["summary_text"]
+    #uncomment to use your model
+    #output=inference_pipeline(text, **gen_kwargs)[0]["summary_text"]
+
+    # Generate Summary from pretrained model
+    inputs = tokenizer([text], max_length=1024, return_tensors="pt")
+    summary_ids = model.generate(inputs["input_ids"])
+    output=tokenizer.batch_decode(summary_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
     return PlainTextResponse(output,200)
